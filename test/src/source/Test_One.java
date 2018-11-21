6d2ab0746abef2a6faa98879059a29d4947087d2
srilatha <kysrilatha@gmail.com>
2018-11-21 10:17:22 +0530

    Initial Project

---
+++ test/src/source/Test_One.java
@@ -0,0 +1,7 @@
+package source;
+
+public class Test_One {
+	public static void main(String[] args) {
+		System.out.println("Test One workspace 1");
+	}
+}
---
+++ test/src/source/Test_Two.java
@@ -0,0 +1,7 @@
+package source;
+
+public class Test_Two {
+	public static void main(String[] args) {
+		System.out.println("Test Two workspace 1");
+	}
+}
